fad8d4fc473a2af4ee6aceda7a2ee0502376aa3f
Gavin Chait <whythawk@gmail.com>
2023-11-29 15:45:45 +0100

    Update PathwayFrontpageSteps.vue

--- frontend/components/pathway/PathwayFrontpageSteps.vue
+++ frontend/components/pathway/PathwayFrontpageSteps.vue
@@ -4,7 +4,7 @@
         <ol role="list" class="overflow-hidden rounded-md lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
           <li v-for="(step, stepIdx) in steps" :key="step.id" class="relative overflow-hidden lg:flex-1">
             <div :class="[stepIdx === 0 ? 'rounded-t-md border-b-0' : '', stepIdx === steps.length - 1 ? 'rounded-b-md border-t-0' : '', 'overflow-hidden border border-gray-200 lg:border-0']">
-                <button class="group">
+                <LocaleLink :to="step.to"  class="group">
                     <span class="absolute left-0 top-0 h-full w-1 bg-transparent group-hover:bg-gray-200 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full" aria-hidden="true" />
                     <span class="flex justify-center py-2 text-sm font-medium">
                         <span class="flex h-8 w-8 items-center justify-center rounded-full border-2 border-kashmir-300">
@@ -14,7 +14,7 @@
                     <span class="mx-3 mb-1 flex min-w-0 flex-col">
                         <span class="text-sm text-gray-500">{{ step.description }}</span>
                     </span>
-                </button>
+                </LocaleLink>
                 <template v-if="stepIdx !== 0">
                     <!-- Separator -->
                     <div class="absolute inset-0 left-0 top-0 hidden w-3 lg:block" aria-hidden="true">
@@ -35,9 +35,9 @@ import { IKeyable } from "@/interfaces"
 
 const { t } = useI18n()
 const steps: IKeyable[] = [
-    { id: "01", description: t("frontpage.steps.one")},
-    { id: "02", description: t("frontpage.steps.two")},
-    { id: "03", description: t("frontpage.steps.three")},
-    { id: "04", description: t("frontpage.steps.four")},
+    { id: "01", description: t("frontpage.steps.one"), to: "/login"},
+    { id: "02", description: t("frontpage.steps.two"), to: "/pathway"},
+    { id: "03", description: t("frontpage.steps.three"), to: "/pathway"},
+    { id: "04", description: t("frontpage.steps.four"), to: "/group"},
 ]
 </script>
